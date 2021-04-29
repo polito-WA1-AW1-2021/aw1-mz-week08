1615a9f40bfe24b8d8ef50a34bcd77a4dd36e463
Luigi De Russis <luigidr@gmail.com>
2021-04-29 10:31:15 +0200

    code developed in class

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 import { Container, Row } from 'react-bootstrap';
 import dayjs from 'dayjs';
 import AppTitle from './components/AppTitle';
-import { ExamScores } from './components/ExamComponents';
+import { ExamScores, ExamForm } from './components/ExamComponents';
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {useState} from 'react';
 
 const fakeExams = [
   {code: '01TYMOV', score: 28, date: dayjs('2021-03-01')},
@@ -25,14 +27,52 @@ const fakeCourses = [
 ];
 
 function App() {
+  const [exams, setExams] = useState([...fakeExams]);
+
+  const examCodes = exams.map(exam => exam.code);
+
+  const deleteExam = (code) => {
+    setExams((exams) => exams.filter(ex => ex.code !== code));
+  };
+
+  const addExam = (exam) => {
+    setExams(exams => [...exams, exam]);
+  }
+
+  const updateExam = (exam) => {
+    setExams(exams => {
+      return exams.map(ex => {
+        if(ex.code === exam.code)
+          return {code: exam.code, score: exam.score, date: exam.date};
+        else
+        return ex;
+      });
+    });
+  }
 
   return (
-    <Container className="App">
-      <AppTitle />
-      <Row>
-        <ExamScores exams={fakeExams} courses={fakeCourses}/>
-      </Row>
-    </Container>
+    <Router>
+      <Container className="App">
+        <AppTitle />
+
+        <Switch>
+          <Route exact path="/" render={() => 
+            <Row>
+              <ExamScores exams={exams} courses={fakeCourses} deleteExam={deleteExam}/>
+            </Row>
+          } />
+
+          <Route path="/add" render={() =>
+            <ExamForm courses={fakeCourses.filter(course => !examCodes.includes(course.code))} addOrUpdateExam={addExam}></ExamForm>
+          }/>
+
+          <Route path="/update" render={() =>
+            <ExamForm courses={fakeCourses} addOrUpdateExam={updateExam}></ExamForm>
+          }/>
+          
+        </Switch>
+      </Container>
+    </Router>
   );
 }
 
--- src/components/ExamComponents.js
+++ src/components/ExamComponents.js
@@ -2,29 +2,17 @@ import {Col, Table, Form, Button, Alert} from 'react-bootstrap';
 import { iconDelete, iconEdit } from '../icons';
 import { useState } from 'react';
 import dayjs from 'dayjs';
+import {Link, Redirect, useLocation} from 'react-router-dom';
 
 function ExamScores(props) {
   return (
     <Col>
-      <ExamTable exams={props.exams} courses={props.courses}/>
+      <ExamTable exams={props.exams} courses={props.courses} deleteExam={props.deleteExam}/>
     </Col>
   );
 }
 
 function ExamTable(props) {
-  const [exams, setExams] = useState([...props.exams]);
-  const [showForm, setShowForm] = useState(false);
-
-  const examCodes = exams.map(exam => exam.code);
-
-  const deleteExam = (code) => {
-    setExams((exams) => exams.filter(ex => ex.code !== code));
-  };
-
-  const addExam = (exam) => {
-    setExams(exams => [...exams, exam]);
-  }
-
   return (
     <>
       <Table striped bordered>
@@ -38,21 +26,21 @@ function ExamTable(props) {
         </thead>
         <tbody>
         {
-          exams.map((ex) => <ExamRow key={ex.code}
+          props.exams.map((ex) => <ExamRow key={ex.code}
             exam={ex}
             examName={props.courses.filter(c => c.code === ex.code)[0].name}
-            deleteExam={deleteExam}
+            deleteExam={props.deleteExam}
           />)
           }
         </tbody>
       </Table>
-      {showForm ? <ExamForm courses={props.courses.filter(course => !examCodes.includes(course.code))} addExam={exam => { addExam(exam); setShowForm(false); }} cancel={() => setShowForm(false)}></ExamForm> : <Button variant='success' onClick = {() => setShowForm(true)}>Add</Button>}
+      <Link to="/add"><Button variant='success'>Add</Button></Link>
     </>
   );
 }
 
 function ExamRow(props) {
-  return <tr><ExamRowData exam={props.exam} examName={props.examName}/><RowControl examCode={props.exam.code} deleteExam={props.deleteExam}/></tr>;
+  return <tr><ExamRowData exam={props.exam} examName={props.examName}/><RowControl exam={props.exam} deleteExam={props.deleteExam}/></tr>;
 }
 
 function ExamRowData(props) {
@@ -65,18 +53,26 @@ function ExamRowData(props) {
 }
 
 function RowControl(props) {
-  return <td>{iconEdit} <span onClick={() => props.deleteExam(props.examCode)}>{iconDelete}</span></td>;
+  return <td>
+    <Link to={{
+      pathname: "/update",
+      state: {exam: props.exam, examDate: props.exam.date.format('YYYY-MM-DD') }
+      }}>{iconEdit}</Link>
+    <span onClick={() => props.deleteExam(props.exam.code)}>{iconDelete}</span>
+  </td>;
 }
 
 function ExamForm(props) {
-  const [course, setCourse] = useState('');
-  const [score, setScore] = useState(30);
-  const [date, setDate] = useState(dayjs().format('YYYY-MM-DD'));
+  const location = useLocation();
+  const [course, setCourse] = useState(location.state ? location.state.exam.code : '');
+  const [score, setScore] = useState(location.state ? location.state.exam.score : 30);
+  const [date, setDate] = useState(location.state ? location.state.examDate : dayjs().format('YYYY-MM-DD'));
   const [errorMessage, setErrorMessage] = useState('');
+  const [submitted, setSubmitted] = useState(false);
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    const newExam = {code: course, score: score, date: dayjs(date)};
+    const exam = {code: course, score: score, date: dayjs(date)};
     // SOME VALIDATION, ADD MORE!!!
     let valid = true;
     if(course === '' || score === '')
@@ -87,7 +83,8 @@ function ExamForm(props) {
     
     if(valid)
     {
-      props.addExam(newExam);
+      props.addOrUpdateExam(exam);
+      setSubmitted(true);
     }
     else {
       // show a better error message...
@@ -96,11 +93,12 @@ function ExamForm(props) {
   }
 
   return(
+    <>{submitted ? <Redirect to="/"/> :
     <Form>
-      {errorMessage ? <Alert variant='danger'>{errorMessage}</Alert> : ''}
+      {errorMessage && <Alert variant='danger'>{errorMessage}</Alert>}
       <Form.Group controlId='selectedCourse'>
         <Form.Label>Course</Form.Label>
-        <Form.Control as='select' defaultValue='' value={course} onChange={event => setCourse(event.target.value)}>
+        <Form.Control as='select' defaultValue='' value={course} onChange={event => setCourse(event.target.value)} disabled={location.state}>
           <option value='' hidden disabled>Choose one...</option>
           { props.courses.map(course => 
           <option key={course.code} value={course.code}>
@@ -119,10 +117,10 @@ function ExamForm(props) {
         <Form.Control type="date" value={date} onChange={event => setDate(event.target.value)} />
       </Form.Group>
 
-      <Button type='submit' onClick={handleSubmit}>Save</Button> <Button variant='secondary' onClick={props.cancel}>Cancel</Button>
+      <Button type='submit' onClick={handleSubmit}>Save</Button> <Link to="/"><Button variant='secondary'>Cancel</Button></Link>
       
-    </Form>
+    </Form>}</>
   )
 }
 
-export {ExamScores};
+export {ExamScores, ExamForm};
